c8f7fd25e6cfc65c76fac33b217be605750364d5
Greg Linklater <greglink49@gmail.com>
2017-07-07 16:51:23 +0200

    get secrets in settings files

--- uchicagohvz/production_settings.py
+++ uchicagohvz/production_settings.py
@@ -8,17 +8,19 @@ ADMINS = (
     ('Administrator', 'greg+hvz@gryphus.io'),
 )
 
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'uchicagohvz',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+try:
+    from secrets import DATABASES
+except:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'uchicagohvz',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
 
 # REST framework settings
 REST_FRAMEWORK = {
@@ -44,3 +46,8 @@ MAIL_USE_TLS = True
 # Chat settings
 CHAT_SERVER_URL = '//hvz.gryphus.io/chat'
 CHAT_ADMIN_URL = '//hvz.gryphus.io/admin'
+
+try:
+    from secrets import GEOPOSITION_GOOGLE_MAPS_API_KEY
+except:
+    GEOPOSITION_GOOGLE_MAPS_API_KEY = 'placeholder'
--- uchicagohvz/settings.py
+++ uchicagohvz/settings.py
@@ -1,11 +1,1 @@
 from production_settings import *
-
-try:
-    from secrets import DATABASES
-except:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
